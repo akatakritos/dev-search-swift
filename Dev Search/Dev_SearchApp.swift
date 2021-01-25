ac8acf76f9d7421c5e74b4b1694ebd2b1e34a5f0
Matt Burke <matt.burke@artisantechgroup.com>
2021-01-25 09:24:21 -0600

    Initial Commit

---
+++ Dev Search/Dev_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_SearchApp.swift
+//  Dev Search
+//
+//  Created by Matt Burke on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Dev_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
